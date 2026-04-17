9725cf9c2e61fa90689961a36cf18c668e5a0796
AditiDekatey <91081262+AditiDekatey@users.noreply.github.com>
2026-04-17 12:02:43 +0530

    Some minor changes

---
+++ Intro.js
@@ -0,0 +1,23 @@
+/*
+ Program - set of instructions that tell a computer what to do in specific circumstances
+ Programming - Process of writing the steps to complete a task
+ Low level languages vs High level languages
+ 
+ Javascript -
+ Builds interactivity in the web
+ Interactive maps, client side form validations,
+
+Direct Interaction - Allows direct interaction with webpages dynamically on the client.
+Backwards compatibility - All websites built in past need work today
+Central pillar - JS baked into the browser, millions of websites 
+Ease of use
+Built in tools
+Used widely - frontend , frrameworks, backend 
+Accessibility & Community
+
+Comments - Express ideas, add to someone else code, team members, to dos , to improve, debugging, understanding,explain, ask questions
+
+Data types - String, Number, Boolean, Null, Undefined, BigInt, Symbol
+
+*/
+console.log('Hello World')
